a524943ab7fc0b490a09f41a210fe603c2330a86
Kamila Ojeda <beau.ojeda@gmail.com>
2023-02-27 08:37:09 -0500

    Agrega dependencias y config de firebase.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App'
 import './index.css'
 
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <div>Hello world!</div>,
+	},
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<RouterProvider router={router} />
+		<App />
+	</React.StrictMode>
+);
