992ccd0e64dd69b3dc2c9f0024fff704e72ae76f
Kamila Ojeda <beau.ojeda@gmail.com>
2023-02-28 23:48:37 -0500

    Agrega estilos a page Login.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -25,7 +25,7 @@ export const Login = (props) => {
 			await signIn(email, password);
 			toNavigate('/home');
 		} catch (error) {
-			setError("setError: " + error.message);
+			setError('setError: ' + error.message);
 			console.log();
 			error.message;
 		}
@@ -37,25 +37,52 @@ export const Login = (props) => {
 	// 	setUser({ ...user, [name]: value });
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<label htmlFor='email'>Correo</label>
-			<input
-				type='email'
-				name='email'
-				placeholder='nombre_tipo@ñañarestaurant.pe'
-				onChange={(e) => setEmail(e.target.value)}
-			/>
-
-			<label htmlFor='password'>Contraseña</label>
-			<input
-				type='password'
-				name='password'
-				id='idPassword'
-				placeholder='**************'
-				onChange={(e) => setPassword(e.target.value)}
-			/>
-
-			<button type='submit' onClick={() => signIn(email, password)}>Iniciar Sesión</button>
-		</form>
+		<div className='flex flex-col items-center justify-center min-h-screen py-1'>
+			<img
+				className='h-96 w-96'
+				src='src/assets/Logo-ñaña.png'
+				alt='imagen del logo de ñaña'
+			></img>
+			<form onSubmit={handleSubmit} classN='w-full max-w-sm'>
+				<div class='mb-4'>
+					<label
+						htmlFor='email'
+						className='block text-gray-700 text-sm font-bold mb-2'
+					>
+						Correo
+					</label>
+					<input
+						type='email'
+						name='email'
+						placeholder='nombre_tipo@ñañarestaurant.pe'
+						onChange={(e) => setEmail(e.target.value)}
+						className='bg-transparent w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none focus:shadow-outline flex items-center border-b border-red-800'
+					/>
+				</div>
+				<div class='mb-6'>
+					<label
+						htmlFor='password'
+						className='block text-gray-700 text-sm font-bold mb-2'
+					>
+						Contraseña
+					</label>
+					<input
+						type='password'
+						name='password'
+						id='idPassword'
+						placeholder='**************'
+						onChange={(e) => setPassword(e.target.value)}
+						className='flex items-center appearance-none bg-transparent w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none border-b border-red-800'
+					/>
+				</div>
+				<button
+					type='submit'
+					onClick={() => signIn(email, password)}
+					className='flex items-center justify-center w-full bg-red-800 hover:bg-red-900 text-white font-bold py-2 px-4 rounded-full'
+				>
+					Iniciar Sesión
+				</button>
+			</form>
+		</div>
 	);
 };
