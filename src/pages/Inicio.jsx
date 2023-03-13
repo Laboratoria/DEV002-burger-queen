f7a839eadfe7370cf61ecf322adf333111eb5724
Kamila Ojeda <beau.ojeda@gmail.com>
2023-03-13 17:28:09 -0500

    Agrega estilos a la page de Inicio.

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -2,11 +2,13 @@ import { useNavigate } from 'react-router-dom';
 const Inicio = () => {
 	const toNavigate = useNavigate();
 	return (
-		<div className='flex items-center justify-center bg-red-800'>
+		<div className='flex flex-col items-center justify-center bg-main w-full min-h-screen'>
+			<figure className='w-3/5'>
+				<img src='src/assets/Logo-La-Ñaña.png' alt='Logo La Ñaña'   />
+			</figure>
 			<button
-				className='flex w-50 bg-red-100 hover:bg-red-900 text-red-800 font-bold py-2 px-4 rounded-full'
 				onClick={() => toNavigate('/login')}
-			>
+				className='flex items-center justify-center bg-secoundary-one rounded-full px-20 py-2 mt-20 text-2xl text-main'>
 				Empezar
 			</button>
 		</div>
