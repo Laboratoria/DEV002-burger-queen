d5f9716d66c9bd91dc2a8a1e7942c7f59e66d937
Erika Suesca <erikasuesca1234@gmail.com>
2023-03-14 12:51:52 -0500

    Se inicia maquetación de la vista Menú

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,24 +1,24 @@
 import React, { Fragment } from "react";
 import Logo from "../Imagenes/logoLasaña.png";
-import './Home.css'
+import './Home.css';
 
 const Home = () => {
   return (
     <Fragment>
       <header className="Home">
         <section className="Logo">
-        <p>LASAGNA</p> 
-        <p>HOUSE</p>
+        <p className="textoLogo">LASAGNA</p> 
+        <p className="textoLogoDos">HOUSE</p>
           <h1><img className="logoLasaña" src={Logo} alt="logoLasagnaHouse" /></h1>
         </section>
 
         <nav className="opciones">
           <section className='btn-nav'>
-            <a href='/menu'><h3>MESERO</h3></a>
+            <a className="opcionMesero" href='/menu'><h3>MESERO</h3></a>
             
           </section>
           <section className='btn-nav'>
-            <a href='/chef'><h3>CHEF</h3></a>
+            <a className="opcionChef" href='/chef'><h3>CHEF</h3></a>
             
           </section>
         </nav>
--- src/Menu/Menu.jsx
+++ src/Menu/Menu.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+import Logo from "../Imagenes/logoLasaña.png";
+import './Menu.css';
 
-const Menu = () =>{
-    return(
-        <div>
-            <p>Soy Menú</p>
-        </div>
+const Menu = () => {
+    return (
+        <>
+            <main className="Menu">
+                <img className="logoLasañaMenu" src={Logo} alt="logoLasagnaHouse" />
+                <a className="opcionInicio" href='/'><h3>INICIO</h3></a>
+                <a className="opcionPedidos" href='/order'><h3>PEDIDOS</h3></a>
+            </main>
+
+            <div className='sect-input'>
+                <input
+                    type='text'
+                    placeholder='NOMBRE DEL CLIENTE'
+                    className='input-name'
+                />
+                <select name='numMesa'className='num-mesa'>
+                    <option disabled selected>MESA</option>
+                    <option value='Mesa1'>Mesa 1</option>
+                    <option value='Mesa2'>Mesa 2</option>
+                    <option value='Mesa3'>Mesa 3</option>
+                    <option value='Mesa4'>Mesa 4</option>
+                    <option value='Mesa5'>Mesa 5</option>
+                </select>
+            </div>
+
+            <footer>@2023 - Erika Ríos Suesca</footer>
+        </>
     )
-}
+};
+
 export default Menu;
