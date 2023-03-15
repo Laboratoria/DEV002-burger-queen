d6aa08f1bff26d9cddd696b665166aa5abed712b
Erika Suesca <erikasuesca1234@gmail.com>
2023-03-15 15:46:24 -0500

    Se realizan cambios en css

--- src/Firebase/fb-config.js
+++ src/Firebase/fb-config.js
@@ -1,21 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCVsxPY6atr0bq03GE3YdXcbV01vqoX-y8",
-  authDomain: "lasagnahouse.firebaseapp.com",
-  projectId: "lasagnahouse",
-  storageBucket: "lasagnahouse.appspot.com",
-  messagingSenderId: "312519870519",
-  appId: "1:312519870519:web:8aca7e8038faabddae57b2",
-  measurementId: "G-ZY09HJKYX5"
+  apiKey: "AIzaSyAM48A5ZLUW-KSXxSWw4gXWY2CYtznRXY4",
+  authDomain: "lasagnahouse-8256f.firebaseapp.com",
+  projectId: "lasagnahouse-8256f",
+  storageBucket: "lasagnahouse-8256f.appspot.com",
+  messagingSenderId: "1071398228744",
+  appId: "1:1071398228744:web:b705f91ca7a40d92d212bd",
+  measurementId: "G-CG8ZDEKW7K"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
+
+export default app;
