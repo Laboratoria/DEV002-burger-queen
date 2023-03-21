ca369ab4c9f02b24644c8fd2e1b4529691ded400
Kamila Ojeda <beau.ojeda@gmail.com>
2023-03-21 13:12:07 -0500

    Agrega hora y fecha en la órdenes enviadas al cheff.

--- src/components/Order/PrintOrder.jsx
+++ src/components/Order/PrintOrder.jsx
@@ -1,13 +1,30 @@
+import {useState} from 'react'
+// import {format} from 'date-fns'
+import { BsCalendarCheck } from 'react-icons/bs';
+import {RxTimer} from 'react-icons/rx'
+
 const PrintOrder = ({ orders }) => {
-	console.log(orders);
+	// orders.forEach(element => {
+	// 	console.log(element);
+	// 	console.log(element.createdAt.toDate());
+	// });
 	return orders.map((order) => (
 		<div
 			key={order.id}
 			className='bg-white border border-main rounded-md shadow-md p-4'>
 			<div className='bg-secoundary-one/50'>
 				<h2>Cliente: {order.client}</h2>
-				<p> Status: {order.status}</p>
-				{/* <p> {order.createdAt.toDate()}</p> */}
+			</div>
+			<div className='bg-secoundary-one/50'>
+				<span className='flex flex-row items-center justify-start'> 
+					<BsCalendarCheck/>
+					<p className='ml-2'>{order.createdAt.toDate().toLocaleDateString()}</p>
+				</span>
+				<span className='flex flex-row items-center justify-start'>
+				<RxTimer/>
+				<p className='ml-2'>{order.createdAt.toDate().toLocaleTimeString()}</p>
+				</span>
+				<p>Status: {order.status}</p>
 			</div>
 			<ul>
 				<h3> Cantidad | Producto </h3>
---
+++ src/pages/ReadyOrders.jsx
@@ -0,0 +1,5 @@
+const ReadyOrders = () => {
+	return <div></div>;
+};
+
+export { ReadyOrders };
