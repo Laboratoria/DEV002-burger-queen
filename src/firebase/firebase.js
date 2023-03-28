cc707edf66249ebe3b35da63b11be29243030d0b
CeToribio <anctoribio@gmail.com>
2023-03-27 21:57:04 -0500

    corregi import en firabse config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,4 @@
-import firebaseConfig from "./firebase-config";
+import {firebaseConfig} from "./firebase-config.js";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
